73654b220e9cf77933ddd4cedacd7bbde32fd0d3
DecebalTech <roboticacnd@gmail.com>
2018-11-04 14:29:22 +0200

    autonomus movement function
    + a function in the movement class that should work with moving the robot using encoders, during the autonomus period

--- Code/org/firstinspires/ftc/teamcode/Movement.java
+++ Code/org/firstinspires/ftc/teamcode/Movement.java
@@ -6,6 +6,7 @@ import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.util.Hardware;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
 
 public class Movement {
@@ -15,6 +16,7 @@ public class Movement {
     public float turbo = 1.0f;
     private float minTurbo = 0.5f, maxTurbo = 2.5f;
     private boolean turboIsPressed = false;
+    public double power = 0.4;
 
 
     public void move(float power) {
@@ -65,5 +67,29 @@ public class Movement {
     public void initMovement(HardwareMap hwm, String moto1Name, String moto2Name) {
         moto1 = hwm.dcMotor.get(moto1Name);
         moto2 = hwm.dcMotor.get(moto2Name);
+    }
+
+    public void moveWithEncoders(int pos, LinearOpMode opmode) {
+
+        moto1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        moto2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        moto1.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        moto2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        moto1.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        moto2.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        moto1.setTargetPosition(pos);
+        moto2.setTargetPosition(pos);
+
+        moto1.setPower(power);
+        moto2.setPower(power);
+
+        while(moto1.isBusy() && moto2.isBusy())
+            opmode.idle();
+            
+        moto1.setPower(0);
+        moto2.setPower(0);
     }
 }
