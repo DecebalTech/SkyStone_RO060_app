45e69aa45d3d3484273018d9f3288095d29ddbb8
DecebalTech <roboticacnd@gmail.com>
2018-11-06 16:22:21 +0200

    Further modularization & VuForia tests
    > moving modules into separate folders
    
    > VuForia concepts added - to study and implement in our own classes later

--- Code/org/firstinspires/ftc/teamcode/BreaslaProgramatorilor.java
+++ Code/org/firstinspires/ftc/teamcode/BreaslaProgramatorilor.java
@@ -3,8 +3,9 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import org.firstinspires.ftc.teamcode.Modules.*;
 
-@TeleOp
+@TeleOp( name = "BreaslaProgramatorilor", group = "Teste")
 
 public class BreaslaProgramatorilor extends LinearOpMode{
 
